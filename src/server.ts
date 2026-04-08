cdb4c81c73de7c5604304a11857ed7b5002fa885
Angel <angel17@gmail.com>
2026-04-08 10:36:26 -0400

    feat: connect frontend with production backend

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 
 dbConnection();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "*" }));
 app.use("/", authRoutes);
 app.use("/", taskRoutes);
 
